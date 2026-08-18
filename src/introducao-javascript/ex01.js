2cc9313af2ddf692e84d88d0415e26a8dafa0376
EdsonRafaelS <eds.rrafael@gmail.com>
2026-08-18 20:31:38 -0300

    Create ex01.js
    Primeiros comando em JavaScript

---
+++ src/introducao-javascript/ex01.js
@@ -0,0 +1,13 @@
+console.log("String com aspas duplas");
+console.log('String com aspas simples');
+console.log(`String com crase`);
+console.log('Com aspas simples "posso ultilzar aspas duplas na frase"')
+
+var nome = 'Rafael';
+let profissao = "Analista";
+const cidade = `Nazaré`;
+
+console.log(nome);
+console.log(profissao);
+console.log(cidade);
+console.log(nome + " residente de " + cidade + " trabalha como "+ profissao);
